b2ed1068fbe1a6be45fb32213651d7a4f356270b
Sandra Jasionowska <35003792+sjasionowska@users.noreply.github.com>
2022-07-26 20:32:11 +0200

    added basic movement to player and fake shooting method

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+
+    private Vector2 _movement;
+
+    [SerializeField]
+    private float _velocity;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+
+    }
+
+
+    private void Update()
+    {
+        _movement.x = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.MovePosition(_rigidbody.position + _movement * (_velocity * Time.fixedDeltaTime));
+    }
+
+    private void Shoot()
+    {
+        Debug.Log("Shoot!");
+    }
+}
