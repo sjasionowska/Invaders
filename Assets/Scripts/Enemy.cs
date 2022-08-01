9b041b0e8d227a23646360f44712b976f7d58e9a
Sandra Jasionowska <35003792+sjasionowska@users.noreply.github.com>
2022-08-01 17:56:22 +0200

    added _shootingProbability with no implementation yet

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,6 +7,13 @@ using UnityEngine;
 
 public class Enemy : Shooter
 {
+    /// <summary>
+    /// Indicates how likely it is for this enemy to shoot when it is loaded.
+    /// </summary>
+    [SerializeField]
+    [Range(0, 100)]
+    private int _shootingProbability;
+
 
 
 }
